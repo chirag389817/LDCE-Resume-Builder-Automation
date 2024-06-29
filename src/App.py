258bedf0e5ee33ca43ec247f7d0c839e47be3d3d
chirag389817 <chirag389817@gmail.com>
2024-06-29 17:28:09 +0530

    added: instruction in comments

--- src/App.py
+++ src/App.py
@@ -1,6 +1,8 @@
 import ResumeBuilder as builder
 
 if __name__ == "__main__":
+    # Remove or comment the section if not want to include
+    # e.g. # builder.fill_hobbies()
     builder.fill_basic_details()
     builder.fill_skills()
     builder.fill_interests()
--- src/Data.py
+++ src/Data.py
@@ -1,3 +1,4 @@
+# Leave empty string "" if not want to include
 profile_path = r"C:\Users\csp38\Desktop\LDCE Resume\3.5 circle.png"
 name = "Chirag Patel"
 email = "chirag389817@gmail.com"
@@ -8,17 +9,23 @@ github_name = "chirag389817"
 github_link = "https://github.com/chirag389817"
 about = "I'm a full stack developer with a passion for Al and machine learning."
 
+# make it empty if not want to include
+# e.g. skills = []
 skills = [
     "HTML",
     "CSS",
     "JavaScript"
 ]
 
+# make it empty if not want to include
+# e.g. area_of_interests = []
 area_of_interests = [
     "Web Development", 
     "Cloud Computing"
 ]
 
+# make it empty if not want to include
+# e.g. educations = []
 educations = [
     {
         "college":"L.D. College of Engineering, Ahmedabad", 
@@ -31,14 +38,19 @@ educations = [
         "college":"AB School, Chikhli", 
         "degree":"Higher Secondary Education", 
         "percentage":"70", 
-        "s_year":"2020", 
+        "s_year":"2020",
+        # Leave empty string if not want to include
+        # e.g. "e_year":""
         "e_year":"2021"
     }
 ]
 
+# make it empty if not want to include
+# e.g. projects = []
 projects = [
     {
         "title":"Quick Quotes",
+        # Leave empty string "" if not want to include
         "link":"",
         "desc":"A quote website where users can add and delete quotes. Built with NextJS and MongoDB, and used NextAuth for user authentication. The home page shows the latest quotes using ISR, and the profile page is dynamic."
     },
@@ -54,18 +66,24 @@ projects = [
     },
 ]
 
+# make it empty if not want to include
+# e.g. experiences = []
 experiences = [
     {
         "company":"Prakalpa Club - LDCE",
         "designation":"Web Development Head",
         "desc":"Managed and updated a Next.js website, making modifications to various pages.",
+        # 1.Jan 2.Feb 3.Mar ... and so on
         "s_month":11,
         "s_year":"2023",
+        # make it 0 if not want to include
+        # e.g. "e_month":0
         "e_month":0,
         "e_year":"present"
     },
     {
         "company":"Smart India Hackathon",
+        # Leave empty string "" if not want to include
         "designation":"",
         "desc":"Developed a dashboard using Next.js to analyze dropout students. Displayed district-wise data on a map of Gujarat.",
         "s_month":12,
@@ -75,11 +93,15 @@ experiences = [
     },
 ]
 
+# make it empty if not want to include
+# e.g. hobbies = []
 hobbies =[
     "Playing Chess", 
     "Solving Sudoku"
 ]
 
+# make it empty if not want to include
+# e.g. achievements = []
 achievements = [
     "AWS Cloud Certified",
     "Python Certification from Courcera",
